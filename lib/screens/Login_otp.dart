a9747cade056cd5728f7138a4f5313e2f18cc2a2
as4656@srmist.edu.in <as4656@srmist.edu.in>
2023-03-14 21:29:37 +0530

    login otp done

--- lib/screens/Login_otp.dart
+++ lib/screens/Login_otp.dart
@@ -39,208 +39,183 @@ class _LoginOtpState extends State<LoginOtp> {
         //centerTitle: true,
         title: Image.asset('assets/images/Group 635.png'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Container(
-              height: size.height * 0.1,
-            ),
-            Text(
-              'Welcome',
-              style: GoogleFonts.openSans(
-                color: '#000000'.toColor(),
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Container(
+                height: size.height * 0.1,
               ),
-            ),
-            Container(
-              height: size.height * 0.1,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 28,
+              Text(
+                'Welcome',
+                style: GoogleFonts.openSans(
+                  color: '#000000'.toColor(),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
                 ),
-                Text(
-                  'Login with Phone no.',
-                  style: GoogleFonts.openSans(
-                    color: '#595959'.toColor(),
-                    fontSize: 26,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              height: size.height * 0.06,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 28,
-                ),
-                Text(
-                  'Enter your mobile number',
-                  style: GoogleFonts.openSans(
-                    color: '#7E7E7E'.toColor(),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              height: size.height * 0,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 6,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: Colors.black.withOpacity(0.13)),
-                  ),
-                  child: Stack(
-                    children: [
-                      Container(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-                        child: InternationalPhoneNumberInput(
-                          onInputChanged: (PhoneNumber number) {
-                            print(number.phoneNumber);
-                          },
-                          onInputValidated: (bool value) {
-                            print(value);
-                          },
-                          /* selectorConfig: SelectorConfig(
-                                selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
-                              ), */
-                          ignoreBlank: false,
-                          //autoValidateMode: AutovalidateMode.disabled,
-                          selectorTextStyle: TextStyle(color: Colors.black),
-                          //textFieldController: controller,
-                          formatInput: false,
-                          maxLength: 9,
-                          keyboardType: TextInputType.numberWithOptions(
-                              signed: true, decimal: true),
-                          cursorColor: Colors.black,
-                          inputDecoration: InputDecoration(
-                            contentPadding:
-                                EdgeInsets.only(bottom: 15, left: 0),
-                            border: InputBorder.none,
-                            hintText: 'Phone Number',
-                            hintStyle: TextStyle(
-                                color: Colors.grey.shade500, fontSize: 16),
-                          ),
-                          /* onSaved: (PhoneNumber number) {
-                                print('On Saved: $number');
-                              }, */
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              height: size.height * 0.045,
-            ),
-            FadeInDown(
-              delay: Duration(milliseconds: 600),
-              child: MaterialButton(
-                minWidth: 258,
-                onPressed: () {
-                  setState(() {
-                    _isLoading = true;
-                  });
-
-                  Future.delayed(Duration(seconds: 2), () {
-                    setState(() {
-                      _isLoading = false;
-                    });
-/*                       Navigator.push(context, MaterialPageRoute(builder: (context) => Verificatoin())); */
-                  });
-                },
-                color: '#D41717'.toColor(),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(9)),
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                child: _isLoading
-                    ? Container(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.white,
-                          color: Colors.black,
-                          strokeWidth: 2,
-                        ),
-                      )
-                    : Text(
-                        "Send",
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                      ),
               ),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            FadeInDown(
-              delay: Duration(milliseconds: 800),
-              child: Row(
+              Container(
+                height: size.height * 0.1,
+              ),
+              Row(
                 children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: '#A9A9A9'.toColor(),
-                    ),
+                  SizedBox(
+                    width: 28,
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(
-                      'Log in with other ways',
-                      style: GoogleFonts.poppins(
-                        color: '#A6A6A6'.toColor(),
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: '#A9A9A9'.toColor(),
+                  Text(
+                    'Login with Phone no.',
+                    style: GoogleFonts.openSans(
+                      color: '#595959'.toColor(),
+                      fontSize: 26,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                /* SizedBox(
+              Container(
+                height: size.height * 0.06,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 28,
+                  ),
+                  Text(
+                    'Enter your mobile number',
+                    style: GoogleFonts.openSans(
+                      color: '#7E7E7E'.toColor(),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: size.height * 0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                    child: IntlPhoneField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 15.0,
+                      ),
+                      initialValue: '456 789 64',
+                      //focusNode: focusNode1,
+                      invalidNumberMessage: 'Invalid Message',
+                      initialCountryCode: 'IN',
+                      onChanged: (phone) {},
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: size.height * 0.045,
+              ),
+              FadeInDown(
+                delay: Duration(milliseconds: 600),
+                child: MaterialButton(
+                  minWidth: 258,
+                  onPressed: () {
+                    setState(() {
+                      _isLoading = true;
+                    });
+
+                    Future.delayed(Duration(seconds: 2), () {
+                      setState(() {
+                        _isLoading = false;
+                      });
+/*                       Navigator.push(context, MaterialPageRoute(builder: (context) => Verificatoin())); */
+                    });
+                  },
+                  color: '#D41717'.toColor(),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(9)),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  child: _isLoading
+                      ? Container(
+                          width: 20,
+                          height: 20,
+                          child: CircularProgressIndicator(
+                            backgroundColor: Colors.white,
+                            color: Colors.black,
+                            strokeWidth: 2,
+                          ),
+                        )
+                      : Text(
+                          "Send",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 90,
+              ),
+              FadeInDown(
+                delay: Duration(milliseconds: 800),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: '#A9A9A9'.toColor(),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Log in with other ways',
+                        style: GoogleFonts.poppins(
+                          color: '#A6A6A6'.toColor(),
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: '#A9A9A9'.toColor(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FadeInDown(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    /* SizedBox(
                               width: 62,
                             ), */
-                IconButton(
-                  icon: Image.asset('assets/images/google.png'),
-                  iconSize: 20,
-                  onPressed: () {},
+                    IconButton(
+                      icon: Image.asset('assets/images/google.png'),
+                      iconSize: 20,
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      width: 28,
+                    ),
+                    IconButton(
+                      icon: Image.asset('assets/images/facebook.png'),
+                      iconSize: 20,
+                      onPressed: () {},
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  width: 28,
-                ),
-                IconButton(
-                  icon: Image.asset('assets/images/facebook.png'),
-                  iconSize: 20,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
